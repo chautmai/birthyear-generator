7f95ce2875b263c90fe0df5bae37b0c237630560
Chau Mai <63076860+chautmai@users.noreply.github.com>
2021-02-06 00:51:39 -0500

    Create birthyear.js

---
+++ birthyear.js
@@ -0,0 +1,25 @@
+function calculate(){
+    var age = document.getElementById('age').value
+
+    if (age == '' || age == 0 ){
+       document.getElementById('year').style.display = 'block';
+       document.getElementById('year').innerHTML = 'Warning: Please enter a valid age..'
+       document.getElementById('year').style.color = 'red';
+       
+    }
+    else{
+        
+        var date = new Date().getFullYear();
+        var birthyear = date - age;
+        
+        document.getElementById('year').style.display = 'block'
+        document.getElementById('year').innerHTML = 'Your birth-year: ' + '<b>' + birthyear + '</b>';
+        
+        
+    }
+}
+
+
+// document.getElementById('prompt').style.display = 'none';
+// document.getElementById('year').style.display = 'none';
+document.getElementById('button').onclick = function(){ calculate(); }
