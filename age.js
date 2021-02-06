31f4e829276c54c4a2aff2f60b7f8939c75bcf08
Chau Mai <63076860+chautmai@users.noreply.github.com>
2021-02-06 01:18:37 -0500

    Update age.js

--- age.js
+++ age.js
@@ -3,23 +3,16 @@ function calculate(){
 
     if (age == '' || age == 0 ){
        document.getElementById('year').style.display = 'block';
-       document.getElementById('year').innerHTML = 'Warning: Please enter a valid age..'
-       document.getElementById('year').style.color = 'red';
-       
+       document.getElementById('year').innerHTML = 'Warning: Please enter a valid age'
+       document.getElementById('year').style.color = 'red';      
     }
-    else{
-        
-        var date = new Date().getFullYear();
-        var birthyear = date - age;
-        
-        document.getElementById('year').style.display = 'block'
-        document.getElementById('year').innerHTML = 'Your birth-year: ' + '<b>' + birthyear + '</b>';
-        
-        
+    else{       
+       var date = new Date().getFullYear();
+       var birthyear = date - age; 
+       document.getElementById('year').style.display = 'block'
+       document.getElementById('year').innerHTML = 'Your birth-year: ' + '<b>' + birthyear + '</b>';        
     }
 }
-
-
 // document.getElementById('prompt').style.display = 'none';
 // document.getElementById('year').style.display = 'none';
 document.getElementById('button').onclick = function(){ calculate(); }
